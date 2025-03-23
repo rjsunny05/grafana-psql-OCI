2d8d98966075f711735115dbf791d7d1b5a3ae47
rjsunnny <rajeevsunny05@gmail.com>
2025-03-23 08:50:33 +0530

    Delete terraform-psql/modules/peering_connection/main.tf

--- terraform-psql/modules/peering_connection/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc_peering_connection" "project_peering" {
-  peer_vpc_id = var.requester_vpc_id
-  vpc_id      = var.accepter_vpc_id
-  auto_accept = true
-  tags = {
-    Name = var.peering_name
-  }
-}
