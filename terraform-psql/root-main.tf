b0aff6c74463a031a0386868bb67f82d7add88a6
rjsunnny <rajeevsunny05@gmail.com>
2025-03-23 08:49:15 +0530

    Update root-main.tf

--- terraform-psql/root-main.tf
+++ terraform-psql/root-main.tf
@@ -8,8 +8,8 @@ module "subnet" {
   source   = "./modules/subnet"
   vpc_id   = module.vpc.vpc_id
   vpc_cidr = "192.168.0.0/16"
-  az1      = "ap-south-1a"
-  az2      = "ap-south-1b"
+  az1      = "us-east-1a"
+  az2      = "us-east-1b"
 }
 
 module "internet_gateway" {
